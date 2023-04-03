70ac3ccafd200735df8c2a0de4bb775463cd30da
DCisHurt <roger33669@gmail.com>
2023-04-03 13:48:05 +0100

    add setting

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
+# MidiBar: a touchbar midi Controller
 
-> Open this page at [https://.github.io//](https://.github.io//)
+## Introduction
 
-## Use as Extension
 
-This repository can be added as an **extension** in MakeCode.
 
-* open []()
-* click on **New Project**
-* click on **Extensions** under the gearwheel menu
-* search for **https://github.com/** and import
-
-## Edit this project ![Build status badge](https://github.com//workflows/MakeCode/badge.svg)
+## Edit this project
 
 To edit this repository in MakeCode.
 
-* open []()
+* open [MakeCode micro:bit](https://makecode.microbit.org/)
 * click on **Import** then click on **Import URL**
-* paste **https://github.com/** and click import
+* paste **<https://github.com/DCisHurt/midiBar>** and click import
 
-## Blocks preview
+## Local Build
 
-This image shows the blocks code from the last commit in master.
-This image may take a few minutes to refresh.
+Install Node.js
 
-![A rendered view of the blocks](https://github.com//raw/master/.github/makecode/blocks.png)
+Install the MakeCode command line tool (add sudo on macOS)
 
-#### Metadata (used for search, rendering)
+```cmd
+npm install -g pxt
+```
 
-* for PXT/
-<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+Install depences
+```cmd
+pxt install
+```
+
+Build project
+```cmd
+pxt build
+```
--- main.ts
+++ main.ts
@@ -3,16 +3,30 @@ serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate31250)
 pins.digitalWritePin(DigitalPin.P8, 1)
 
 let lastA = false
-let lastB = false
 
 let noteA = 35
-let noteB = 39
 let velocity = 127
 
 let lastAng = 0
-let lastAng2 = 0
 let channel_1 = 1
-let channel_2 = 2
+
+let touchBuff: number[];
+let noteBuff: number[];
+let precision = 12;
+let bitDepth = Math.pow(2, precision);
+let baseNote = 36;
+let octaveScale = 3;
+let lastTouch = 0;
+
+Trill.init(
+    TrillDevice.TRILL_BAR,
+    TrillSpeed.ULTRA_FAST,
+    TrillMode.AUTO,
+    precision,
+    1,
+    10
+) 
+
 
 const noteOnByte = 0x90
 const noteOffByte = 0x80
@@ -21,10 +35,7 @@ const midiCCByte = 0xB0
 
 basic.forever(function () {
     let A = input.buttonIsPressed(Button.A)
-    let B = input.buttonIsPressed(Button.B)
-
     let ang = pins.analogReadPin(AnalogPin.P1)
-    let ang2 = pins.analogReadPin(AnalogPin.P2)
 
     if (A && !lastA) {
         noteOn(channel_1, noteA, velocity)
@@ -35,27 +46,66 @@ basic.forever(function () {
         pins.digitalWritePin(DigitalPin.P8, 1)
     }
         
-    if (B && !lastB) {
-        noteOn(channel_2, noteB, velocity)
-    }
-    else if (!B && lastB) {
-        noteOff(channel_2, noteB)
-    }
     if (Math.abs(lastAng - ang) >= 2) {
         let pitch = (ang - 511) * 16
         pitchBend(channel_1, pitch)
     }
-    if (Math.abs(lastAng2 - ang2) >= 2) {
-        let pitch2 = (ang - 511) * 16
-        pitchBend(channel_2, pitch2)
+    let touch = Trill.numTouchRead();
+
+    if (touch <= lastTouch) {
+        for (let i = 0; i <= lastTouch; i++) {
+            for (let j = 0; j <= touch; j++) {
+                // pitchbend update
+                let temp = Trill.touchCoordinate(j);
+                if (Math.abs(temp - noteBuff[i]) < 100) {
+                    pitchBend(i, temp - noteBuff[i]);
+                    break;
+                }
+                // note off
+                if (j == touch) {
+                    noteOff(i, data2note(noteBuff[i]));
+                    pitchBend(i, 0);
+                    noteBuff[i] = -1;
+                }
+            }
+        }
+    }
+    else {
+        for (let i = 0; i <= touch; i++) {
+            let temp = Trill.touchCoordinate(i);
+            for (let j = 0; j <= lastTouch; j++) {
+                // pitchbend update
+                if (Math.abs(temp - noteBuff[j]) < 100) {
+                    pitchBend(j, temp - noteBuff[j]);
+                    break;
+                }
+                // new note on
+                if (j == lastTouch) {
+                    let newChannel = lastTouch + 1;
+                    noteBuff[newChannel] = note2data(data2note(temp));
+
+                    noteOn(newChannel, data2note(noteBuff[newChannel]), 127);
+                    pitchBend(newChannel, temp - noteBuff[newChannel]);
+                }
+            }
+        }
     }
 
+    lastTouch = touch;
     lastAng = ang
-    lastAng2 = ang2
     lastA = A
-    lastB = B
-    basic.pause(10)
+    basic.pause(1)
 })
+
+
+function note2data(note: number): number {
+    return (note - baseNote) * bitDepth / (12 * octaveScale);
+}
+
+function data2note(data: number): number {
+    return (data * 12 * octaveScale / bitDepth) + baseNote;
+}
+
 
 function midiCC(ch: number, num: number, value: number): void{
     let msg = Buffer.create(3)
@@ -95,68 +145,3 @@ function pitchBend(ch: number, shift: number): void{
     serial.writeBuffer(msg)
 }        
 
-
-
-// // tests go here; this will not be compiled when this package is used as an extension.
-
-// basic
-// let touchBuff: number[];
-// let noteBuff: number[];
-
-// let lastTouch = 0;
-
-// Trill.init(
-//     TrillDevice.TRILL_BAR,
-//     TrillSpeed.TRILL_SPEED_ULTRA_FAST,
-//     TrillMode.AUTO,
-//     12,
-//     1,
-//     10
-// ) 
-
-// basic.forever(function () {
-//     Trill.read();
-
-//     let touch = Trill.numTouchRead();
-
-//     if (touch <= lastTouch) {
-//         for (let i = 0; i <= lastTouch; i++) {
-//             for (let j = 0; j <= touch; j++) {
-//                 // pitchbend update
-//                 let temp = Trill.touchRead(j);
-//                 if (Math.abs(temp - noteBuff[i]) < 100) {
-//                     pitchBend(i, temp - noteBuff[i]);
-//                     break;
-//                 }
-//                 // note off
-//                 if (j == touch) {
-//                     noteOff(i, data2note(noteBuff[i]));
-//                     pitchBend(i, 0);
-//                     noteBuff[i] = -1;
-//                 }
-//             }
-//         }
-//     }
-//     else {
-//         for (let i = 0; i <= touch; i++) {
-//             let temp = Trill.touchRead(i);
-//             for (let j = 0; j <= lastTouch; j++) {
-//                 // pitchbend update
-//                 if (Math.abs(temp - noteBuff[j]) < 100) {
-//                     pitchBend(j, temp - noteBuff[j]);
-//                     break;
-//                 }
-//                 // new note on
-//                 if (j == lastTouch) {
-//                     let newChannel = lastTouch + 1;
-//                     noteBuff[newChannel] = note2data(data2note(temp));
-
-//                     noteOn(newChannel, data2note(noteBuff[newChannel]), 127);
-//                     pitchBend(newChannel, temp - noteBuff[newChannel]);
-//                 }
-//             }
-//         }
-//     }
-
-//     lastTouch = touch;
-// })
